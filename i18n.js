406def071fb96ea88577468f89b959f339e57016
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2026-09-08 10:03:58 -0300

    fix: load residual translation QA layer

--- i18n.js
+++ i18n.js
@@ -1,8 +1,16 @@
 (() => {
   'use strict';
   const inPresentation = /\/apresentacao\/?(?:$|[?#])/.test(location.pathname + location.search + location.hash) || location.pathname.includes('/apresentacao/');
-  const script = document.createElement('script');
-  script.src = `${inPresentation ? '../' : ''}i18n-complete.js?v=20260908-2`;
-  script.defer = true;
-  document.head.appendChild(script);
+  const prefix = inPresentation ? '../' : '';
+
+  const core = document.createElement('script');
+  core.src = `${prefix}i18n-complete.js?v=20260908-3`;
+  core.defer = true;
+  core.onload = () => {
+    const fixes = document.createElement('script');
+    fixes.src = `${prefix}i18n-fixes.js?v=20260908-1`;
+    fixes.defer = true;
+    document.head.appendChild(fixes);
+  };
+  document.head.appendChild(core);
 })();
