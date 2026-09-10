daa84a0abd58a1976f721255e0b762ddd461863c
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2026-09-10 09:59:54 -0300

    refactor: route presentation to semantic i18n engine

--- i18n.js
+++ i18n.js
@@ -1,14 +1,21 @@
 (() => {
   'use strict';
-  const inPresentation = /\/apresentacao\/?(?:$|[?#])/.test(location.pathname + location.search + location.hash) || location.pathname.includes('/apresentacao/');
-  const prefix = inPresentation ? '../' : '';
+  const inPresentation = location.pathname.includes('/apresentacao/');
+
+  if (inPresentation) {
+    const presentationI18n = document.createElement('script');
+    presentationI18n.src = 'presentation-i18n.js?v=20260910-v3';
+    presentationI18n.defer = true;
+    document.head.appendChild(presentationI18n);
+    return;
+  }
 
   const core = document.createElement('script');
-  core.src = `${prefix}i18n-complete.js?v=20260908-3`;
+  core.src = 'i18n-complete.js?v=20260908-3';
   core.defer = true;
   core.onload = () => {
     const fixes = document.createElement('script');
-    fixes.src = `${prefix}i18n-fixes.js?v=20260908-2`;
+    fixes.src = 'i18n-fixes.js?v=20260908-2';
     fixes.defer = true;
     document.head.appendChild(fixes);
   };
