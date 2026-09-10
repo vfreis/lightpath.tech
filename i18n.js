255806298117f90e5b27617a63bb41aa597dd9ee
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2026-09-10 09:33:39 -0300

    fix: avoid duplicate presentation gesture guard injection

--- i18n.js
+++ i18n.js
@@ -3,13 +3,6 @@
   const inPresentation = /\/apresentacao\/?(?:$|[?#])/.test(location.pathname + location.search + location.hash) || location.pathname.includes('/apresentacao/');
   const prefix = inPresentation ? '../' : '';
 
-  if (inPresentation) {
-    const guard = document.createElement('script');
-    guard.src = `gesture-guard.js?v=20260910-1`;
-    guard.defer = true;
-    document.head.appendChild(guard);
-  }
-
   const core = document.createElement('script');
   core.src = `${prefix}i18n-complete.js?v=20260908-3`;
   core.defer = true;
